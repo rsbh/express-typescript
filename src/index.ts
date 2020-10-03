055b0c3428036151408c5a961b955f190c6750b5
Rishabh Mishra <me@rishabhmishra.dev>
2020-10-03 22:53:33 +0530

    Install dependencies and add basic server code

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Application } from "express";
+
+const PORT = process.env.PORT || 8000;
+
+const app: Application = express();
+
+app.get("/ping", async (_req, res) => {
+  res.send({
+    message: "pong",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
