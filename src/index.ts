4cda419d3748c7324011bef950b5fa85009e1472
Rishabh Mishra <me@rishabhmishra.dev>
2020-10-03 22:59:06 +0530

    add express middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express, { Application } from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
+
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
 
 app.get("/ping", async (_req, res) => {
   res.send({
