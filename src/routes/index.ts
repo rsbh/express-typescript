558221cf62d7d56321c7ea97e0782faa1661192e
Rishabh Mishra <me@rishabhmishra.dev>
2020-12-22 17:24:02 +0530

    feat: add create user, get user and get all users apis

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
 import PingController from "../controllers/ping";
+import UserController from "../controllers/user";
 
 const router = express.Router();
 
 router.get("/ping", async (_req, res) => {
   const controller = new PingController();
   const response = await controller.getMessage();
+  return res.send(response);
+});
+
+router.get("/users", async (_req, res) => {
+  const controller = new UserController();
+  const response = await controller.getUsers();
+  return res.send(response);
+});
+
+router.post("/users", async (req, res) => {
+  const controller = new UserController();
+  const response = await controller.createUser(req.body);
+  return res.send(response);
+});
+
+router.get("/users/:id", async (req, res) => {
+  const controller = new UserController();
+  const response = await controller.getUser(req.params.id);
+  if (!response) res.status(404).send({message: "No user found"})
   return res.send(response);
 });
 
