a58972d8be51a02244a3622ff0f612e3c753abfe
Rishabh Mishra <me@rishabhmishra.dev>
2021-01-10 21:25:48 +0530

    test: add test for createUser and getUser function

--- src/controllers/user.controller.test.ts
+++ src/controllers/user.controller.test.ts
@@ -1,6 +1,6 @@
 import UserController from './user.controller'
 import * as UserRepository from '../repositories/user.repository'
-import {generateUsers} from 'test/utils/generate'
+import {generateUsersData, generateUserPayload, generateUserData} from 'test/utils/generate'
 
 afterEach(() => {
   jest.resetAllMocks()
@@ -17,11 +17,49 @@ describe("UserController", () => {
     })
 
     test("should return user list", async () => {
-      const usersList = generateUsers(2)
-      const spy = jest.spyOn(UserRepository, 'getUsers').mockResolvedValueOnce(usersList)
+      const usersData = generateUsersData(2)
+      const spy = jest.spyOn(UserRepository, 'getUsers').mockResolvedValueOnce(usersData)
       const controller = new UserController();
       const users = await controller.getUsers();
-      expect(users).toEqual(usersList)
+      expect(users).toEqual(usersData)
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe("addUser", () => {
+    test("should add user to the database", async () => {
+      const payload = generateUserPayload()
+      const userData = generateUserData(payload)
+      const spy = jest.spyOn(UserRepository, 'createUser').mockResolvedValueOnce(userData)
+      const controller = new UserController();
+      const user = await controller.createUser(payload);
+      expect(user).toMatchObject(payload)
+      expect(user).toEqual(userData)
+      expect(spy).toHaveBeenCalledWith(payload)
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe("getUser", () => {
+    test("should return user from the database", async () => {
+      const id = 1
+      const userData = generateUserData({id})
+      const spy = jest.spyOn(UserRepository, 'getUser').mockResolvedValueOnce(userData)
+      const controller = new UserController();
+      const user = await controller.getUser(id.toString());
+      expect(user).toEqual(userData)
+      expect(user?.id).toBe(id)
+      expect(spy).toHaveBeenCalledWith(id)
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
+
+    test("should return null if user not found", async () => {
+      const id = 1
+      const spy = jest.spyOn(UserRepository, 'getUser').mockResolvedValueOnce(null)
+      const controller = new UserController();
+      const user = await controller.getUser(id.toString());
+      expect(user).toBeNull()
+      expect(spy).toHaveBeenCalledWith(id)
       expect(spy).toHaveBeenCalledTimes(1)
     })
   })
--- test/utils/generate.ts
+++ test/utils/generate.ts
@@ -1,16 +1,31 @@
 import faker from 'faker'
 
-export function generateUsers(n: number = 1) {
-  return Array.from({ length: n }, (_, i) => {
-    return {
-      id: i,
-      firstName:  faker.name.firstName(),
-      lastName:  faker.name.lastName(),
-      email: faker.internet.email(),
-      posts: [],
-      comments: [],
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
+export function generateUserData(overide = {}) {
+  return {
+    id: faker.random.number(),
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+    posts: [],
+    comments: [],
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...overide
+  }
+}
+
+export function generateUsersData(n: number = 1) {
+  return Array.from({
+    length: n
+  }, (_, i) => {
+    return generateUserData({id: i})
   });
+}
+
+export function generateUserPayload() {
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    email: faker.internet.email(),
+  }
 }
