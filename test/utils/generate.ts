ba0ac20d4c398da803738ce59387f9d02886afca
Rishabh Mishra <me@rishabhmishra.dev>
2021-02-14 21:53:13 +0530

    test: add tests for comment controller

--- test/utils/generate.ts
+++ test/utils/generate.ts
@@ -1,5 +1,5 @@
 import faker from 'faker'
-import {  User } from '../../src/models';
+import { User, Post } from '../../src/models';
 
 export function generateUserData(overide = {}) {
   return {
@@ -58,5 +58,36 @@ export function generatePostPayload() {
     title: faker.lorem.sentence(),
     content: faker.lorem.paragraph(),
     userId: faker.random.number(),
+  }
+}
+
+export function generateCommentData(overide = {}) {
+  return {
+    id: faker.random.number(),
+    content: faker.lorem.paragraph(),
+    userId: faker.random.number(),
+    user: new User(),
+    postId: faker.random.number(),
+    post: new Post(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...overide
+  }
+}
+
+export function generateCommentsData(n: number = 1, overide = {}) {
+  return Array.from({
+    length: n
+  }, (_, i) => {
+    return generateCommentData(overide)
+  });
+}
+
+
+export function generateCommentPayload() {
+  return {
+    content: faker.lorem.paragraph(),
+    userId: faker.random.number(),
+    postId: faker.random.number(),
   }
 }
