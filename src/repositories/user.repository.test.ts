3a932cea78729e617b9cb565ebfe0310ba15e217
Rishabh Mishra <me@rishabhmishra.dev>
2021-02-20 23:22:44 +0530

    test: add tests for user repo getUser function

--- src/repositories/user.repository.test.ts
+++ src/repositories/user.repository.test.ts
@@ -7,7 +7,8 @@ jest.mock('typeorm', () => {
   return {
     getRepository: jest.fn().mockReturnValue({
       find: jest.fn(),
-      save: jest.fn()
+      save: jest.fn(),
+      findOne: jest.fn()
     }),
     PrimaryGeneratedColumn: jest.fn(),
     Column: jest.fn(),
@@ -22,6 +23,8 @@ jest.mock('typeorm', () => {
 const mockedGetRepo = mocked(getRepository(<jest.Mock>{}))
 beforeEach(() => {
   mockedGetRepo.find.mockClear()
+  mockedGetRepo.findOne.mockClear()
+  mockedGetRepo.save.mockClear()
 })
 
 describe("UserRepository", () => {
@@ -57,4 +60,25 @@ describe("UserRepository", () => {
     })
   })
 
+  describe("getUser", () => {
+    test("should return user from the database", async () => {
+      const id = 1
+      const userData = generateUserData({id})
+      mockedGetRepo.findOne.mockResolvedValue(userData)
+      const user = await UserRepository.getUser(id)
+      expect(user).toEqual(userData)
+      expect(user?.id).toBe(id)
+      expect(mockedGetRepo.findOne).toHaveBeenCalledWith({id})
+      expect(mockedGetRepo.findOne).toHaveBeenCalledTimes(1)
+    })
+
+    test("should return null if user not found", async () => {
+      const id = 1
+      mockedGetRepo.findOne.mockResolvedValue(null)
+      const user = await UserRepository.getUser(id)
+      expect(user).toBeNull()
+      expect(mockedGetRepo.findOne).toHaveBeenCalledWith({id})
+      expect(mockedGetRepo.findOne).toHaveBeenCalledTimes(1)
+    })
+  })
 })
